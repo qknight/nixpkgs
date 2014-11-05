e9b20153b9f332ff78c87e67936c4f2d47e36aea
Joachim Schiele <js@lastlog.de>
2014-11-05 17:47:54 +0100

    pyqwt from debian does not solve anything. new: packaged examples from pyqwt which fail with the same reason as brickv

--- pkgs/development/libraries/qwt/default.nix
+++ pkgs/development/libraries/qwt/default.nix
@@ -1,16 +1,21 @@
 { stdenv, fetchurl, qt4 }:
 
 stdenv.mkDerivation rec {
-  name = "qwt-5.2.1";
+  name = "qwt-5.2.3";
 
   src = fetchurl {
-    url = "mirror://sourceforge/qwt/${name}.tar.bz2";
-    sha256 = "17snmh8qwsgb4j2yiyzmi0s1jli14vby5wv1kv4kvjq4aisvpf72";
+    url = "http://archive.ubuntu.com/ubuntu/pool/universe/q/qwt5/qwt5_5.2.3.orig.tar.gz";
+    sha256 = "5303941f265ba02e9b2144a6ca3556aaa48dda7b5225505ab9db549032efecd0";
   };
+
+  #src = fetchurl {
+  #  url = "mirror://sourceforge/qwt/${name}.tar.bz2";
+  #  sha256 = "17snmh8qwsgb4j2yiyzmi0s1jli14vby5wv1kv4kvjq4aisvpf72";
+  #};
 
   propagatedBuildInputs = [ qt4 ];
 
-  patches = [ ./prefix.diff ];
+  patches = [ ./prefix-5.2.3.diff ];
 
   postPatch = ''
     sed -e "s@\$\$\[QT_INSTALL_PLUGINS\]@$out/lib/qt4/plugins@" -i designer/designer.pro
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4635,7 +4635,8 @@ let
 
   pyqwt = buildPythonPackage rec {
     name = "pyqwt-${version}";
-    version = "5.2.0";
+    #version = "5.2.0";
+    version = "5.2.1";
 
     src = pkgs.fetchurl {
       url = "https://lastlog.de/misc/pyqwt5_5.2.1-cvs20091107+dfsg.orig.tar.gz";
@@ -4649,13 +4650,12 @@ let
 
     doCheck = false;
 
-    buildInputs = [ pkgs.pyqt4 pkgs.qwt ];
-    configurePhase = ''
-      # forcefully prevent usage of bundled 5.2.0 version of qwt
-      rm -Rf qwt-5.2
+    buildInputs = with self; [ pkgs.pyqt4 pkgs.qwt numpy ];
+    propagatedBuildInputs = with self; [ numpy pyqt4 ];
 
+    configurePhase = ''
       cd configure
-      python configure.py -Q ${pkgs.qwt} --module-install-path=$out/lib/python2.7/site-packages/Qwt5 -I${pkgs.qwt}/include/qwt
+      python configure.py -Q ${pkgs.qwt} --module-install-path=$out/lib/python2.7/site-packages/Qwt5 -I ${pkgs.qwt}/include/qwt
     '';
 
     buildPhase = ''
@@ -4666,21 +4666,22 @@ let
                 "$out"
       cd ..
       # rename from PyQt4.Qwt5 to Qwt5 (since we don't install it into the PyQt4 store path!
-      #for i in configure/qwt5qt4/ipy_user_conf.py configure/tmp-qwt5qt4/ipy_user_conf.py qt3examples/CurveDemo2.py qt3examples/CPUplot.py qt3examples/CurveDemo3.py qt4examples/ErrorBarDemo.py qt4examples/EventFilterDemo.py qt4examples/CurveDemo1.py qt4examples/ImagePlotDemo.py qt4examples/SliderDemo.py qt4examples/CurveDemo2.py qt4examples/SimpleDemo.py qt4examples/DataDemo.py qt4examples/MapDemo.py qt4examples/DialDemo.py qt4examples/Grab.py qt4examples/CPUplot.py qt4examples/BodeDemo.py qt4examples/RadioDemo.py qt4examples/MultiDemo.py qt4examples/HistogramDemo.py qt4examples/ReallySimpleDemo.py qt4examples/SpectrogramDemo.py qt4examples/CartesianDemo.py qt4examples/CurveDemo3.py qt4examples/MaskedDataDemo.py qt4examples/BarPlotDemo.py qt4lib/PyQt4/Qwt5/ipy_user_conf.py sphinx/build/html/_sources/reference.txt sphinx/reference.rst; do
-      #  substituteInPlace $i --replace "PyQt4.Qwt5" "Qwt5"
-      #done
-      # if the above lines are activated, this error occures then:
-      #cat: sphinx/build/html/_sources/reference.txt: No such file or directory
-      #/nix/store/q4kb35wfj35893z8qddlil98yc1akkgc-stdenv/setup: line 363: sphinx/build/html/_sources/reference.txt.tmp: No such file or directory
-      #note: keeping build directory `/tmp/nix-build-python2.7-pyqwt-5.2.0.drv-12'
+      find . -name \*.py | while read i
+      do
+        sed -i -e "s|PyQt4.Qwt5|Qwt5|" $i
+      done
     '';
 
     installPhase = ''
+      mkdir -p $out/examples
+      cp -R qt4examples/* $out/examples/
       cd configure
       make install
-      echo "Qwt5/" > $out/lib/python2.7/site-packages/Qwt.pth
-      echo "Qwt5/" > $out/lib/python2.7/site-packages/Qwt5.pth
+      #echo "Qwt5/" > $out/lib/python2.7/site-packages/Qwt.pth
+      #echo "Qwt5/" > $out/lib/python2.7/site-packages/Qwt5.pth
     '';
+
+    postFixup = "wrapPythonPrograms";
 
     meta = {
       homepage = https://sourceforge.net/projects/pyqwt/;
