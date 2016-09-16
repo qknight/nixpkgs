440d6d8000fe067b4388cef31860a9accb6930d6
Joachim Schiele <js@lastlog.de>
2016-09-16 21:15:19 +0200

    leaps: revision bump to master with subdirectory feature

--- pkgs/development/tools/leaps/default.nix
+++ pkgs/development/tools/leaps/default.nix
@@ -3,14 +3,14 @@
 buildGoPackage rec {
   name = "leaps-${version}";
   version = "20160626-${stdenv.lib.strings.substring 0 7 rev}";
-  rev = "5cf7328a8c498041d2a887e89f22f138498f4621";
+  rev = "5d19e3f44a88d0f8f5ea058b0998ea351c7cac64";
 
   goPackagePath = "github.com/jeffail/leaps";
 
   src = fetchgit {
     inherit rev;
-    url = "https://github.com/jeffail/leaps";
-    sha256 = "1qbgz48x9yi0w9yz39zsnnhx5nx2xmrns9v8hx28jah2bvag6sq7";
+    url = "https://github.com/qknight/leaps";
+    sha256 = "1pwjmg6k227vfgklw73dj10hvf005vy1gh6kfhrqipvvx1q0bjvf";
     fetchSubmodules = false;  
   };
 
